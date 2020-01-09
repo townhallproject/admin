49f89dd5f7bdbffd1f7aa33406f32f4a5bb98a41
meganrm <meganrm@gmail.com>
2020-01-08 22:31:10 -0800

    added override

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV  || 'development';
+console.log('start', process.env.NODE_ENV)
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
